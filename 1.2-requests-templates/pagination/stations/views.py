3d2a4e36972020680ceacdc7f14239d9d0c9eb1a
Aidar <aidar_tn@icloud.com>
2025-04-01 01:42:49 +0300

    Изменение в функции

--- 1.2-requests-templates/pagination/stations/views.py
+++ 1.2-requests-templates/pagination/stations/views.py
@@ -4,13 +4,13 @@ from django.core.paginator import Paginator
 import csv
 from pagination.settings import BUS_STATION_CSV
 
+
 def index(request):
     return redirect(reverse("bus_stations"))
 
 
-
 BUS_STATIONS = []
-with open(BUS_STATION_CSV, mode='r',encoding='utf-8') as file:
+with open(BUS_STATION_CSV, mode="r", encoding="utf-8") as file:
     reader = csv.DictReader(file)
 
     for row in reader:
@@ -20,14 +20,13 @@ with open(BUS_STATION_CSV, mode='r',encoding='utf-8') as file:
 def bus_stations(request):
     # получите текущую страницу и передайте ее в контекст
     # также передайте в контекст список станций на странице
-    
 
     page_number = int(request.GET.get("page", 1))
     paginator = Paginator(BUS_STATION_CSV, 10)
     page = paginator.get_page(page_number)
 
     context = {
-        "bus_stations": BUS_STATIONS,
-        "page": page,
+        "bus_stations": page,
+        "page": page_number,
     }
     return render(request, "stations/index.html", context)
